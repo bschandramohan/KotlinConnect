6855af1c9d667ffa2f8ae8ef1bdcdea886e6243e
bschandramohan <bschandramohan@gmail.com>
2020-11-20 05:57:00 -0800

    More calls added

--- src/main/kotlin/com/bschandramohan/learn/kotlinconnect/Lambdas.kt
+++ src/main/kotlin/com/bschandramohan/learn/kotlinconnect/Lambdas.kt
@@ -7,8 +7,8 @@ var sum = { a: Int, b: Int -> a + b }
 var diff = { a: Int, b: Int -> a - b }
 
 // Higher order function - taking a function as argument
-fun operation (a: Int, b: Int, operate: (Int, Int) -> Int ) : Int {
-    return operate(a, b)
+fun operation (a: Int, b: Int, operation: (Int, Int) -> Int) : Int {
+    return operation(a, b)
 }
 
 // Lambda Expression that takes a function as parameter
@@ -17,12 +17,22 @@ var operation0 = { a: Int, b: Int, operate: (Int, Int) -> Int -> operate(a, b) }
 // Function literal with inline lambda definition
 var operation1: (Int, Int, (Int, Int) -> Int) -> Int = { a, b, operate -> operate(a, b) }
 
+
+var a = {}
+
+
 fun main(args: Array<String>) {
     println(operation(2, 3, sum))
     println(operation(8, 5, diff))
+    operation(8, 5, { a: Int, b: Int -> a - b })
+    operation(8, 5) { a: Int, b: Int -> a - b }
+
+
+    println(operation(5, 8, { x, y -> x * y }))
     println(operation(5, 8) { x, y -> x * y })
+
     println(operation0(5, 8, sum))
     println(operation1(5, 8, sum))
-
 }
+
 
